58204568dae0d0cd3d90c5c8ba8078dff25c6d72
Jose Gustavo Valladares Barahona <gusbarahona01@gmail.com>
2023-03-24 19:46:33 -0600

    Arreglo de location.href

--- Servicios/Sesion.js
+++ Servicios/Sesion.js
@@ -2,18 +2,19 @@
 const validar = () =>{     
   let correo = document.getElementById("correo").value;
   let password = document.getElementById("password").value;
+  let rescorreo = "";
+  let respass = "";
       
     fetch("http://localhost:5000/Admin")
       .then(res => res.json())
       .then(json => { 
          json.map(dato =>{
-          if(dato.correo === correo && dato.password === password){
+           if(dato.correo === correo || dato.password === password){
             alert("Acceso concedido");
-            window.location.href="Menu-Admin.html"
+            window.open("Menu-Admin.html")
           }else{
             alert("Su usuario o contraseña es incorrecto, intente de nuevo");
           }
-          
         })
       })
 }
